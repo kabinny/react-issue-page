f6bbce8879eaa5f45b02f064c1d8aa193a90daab
kabinny <kabinny@gmail.com>
2022-09-11 02:08:39 +0900

    useEffect 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import "./App.css"
+
+// useEffect
+/**
+ * mount ( = 리액트 컴포넌트가 그려질 떄, 렌더될 때)
+ * unmount ( = 리액트 컴포넌트가 사라질 때, 지워질 때)
+ * update ( = 특정 값이 변해서 리액트 컴포넌트가 다시 그려질 때)
+ *
+ */
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    if (count === 5) {
+      console.log("This is five")
+    }
+  }, [count])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>{count}</span>
+      <button onClick={() => setCount(count + 1)}>+</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
