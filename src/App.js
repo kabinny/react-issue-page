f6405e93e43df0a7eb02d376d59ed72052b47e23
kabinny <kabinny@gmail.com>
2022-09-11 16:47:03 +0900

    input 만들기

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import styles from "./App.module.css"
-import Button from "./components/Button"
 import Header from "./Header"
+import ListContainer from "./ListContainer"
 
 function App() {
   return (
     <>
       <div className={styles.nav}>Nav</div>
       <Header />
-      <div className={styles.listContainer}>
-        <Button
-          style={{ fontSize: "14px", backgroundColor: "green", color: "white" }}
-        >
-          New Issue
-        </Button>
-      </div>
+      <ListContainer />
       <div className={styles.footer}>footer</div>
     </>
   )
