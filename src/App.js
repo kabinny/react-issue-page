856cc49979a755d181508bb70dd6a9fee7071a23
kabinny <kabinny@gmail.com>
2022-09-11 14:51:32 +0900

    버튼 만들기

--- src/App.js
+++ src/App.js
@@ -4,11 +4,59 @@ function App() {
   return (
     <>
       <div className={styles.nav}>Nav</div>
-      <div className={styles.header}>Header</div>
-      <div className={styles.listContainer}>listContainer</div>
+      <div className={styles.header}>
+        <Button
+          style={{
+            fontSize: "14px",
+            backgroundColor: "transparent",
+            color: "black",
+          }}
+        >
+          Watch <div className={styles.circle}>5</div>
+        </Button>
+        <Space />
+        <Button
+          style={{
+            fontSize: "14px",
+            backgroundColor: "transparent",
+            color: "black",
+          }}
+        >
+          Fork
+        </Button>
+        <Space />
+        <Button
+          style={{
+            fontSize: "14px",
+            backgroundColor: "transparent",
+            color: "black",
+          }}
+        >
+          Star
+        </Button>
+      </div>
+      <div className={styles.listContainer}>
+        <Button
+          style={{ fontSize: "14px", backgroundColor: "green", color: "white" }}
+        >
+          New Issue
+        </Button>
+      </div>
       <div className={styles.footer}>footer</div>
     </>
   )
+}
+
+function Button({ style, children }) {
+  return (
+    <button className={styles.button} style={style}>
+      {children}
+    </button>
+  )
+}
+
+function Space() {
+  return <div className={styles.space}></div>
 }
 
 export default App
