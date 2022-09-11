0048bf5556111106e812b40b3b76c5dd131b6603
kabinny <kabinny@gmail.com>
2022-09-11 12:58:17 +0900

    페이지 레이아웃 구현

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
-import { useEffect, useState } from "react"
-import "./App.css"
-
-// useEffect
-/**
- * mount ( = 리액트 컴포넌트가 그려질 떄, 렌더될 때)
- * unmount ( = 리액트 컴포넌트가 사라질 때, 지워질 때)
- * update ( = 특정 값이 변해서 리액트 컴포넌트가 다시 그려질 때)
- *
- */
-
-// hook 사용 규칙
-// 1. 최상위에서 호출되어야 한다.
-// 2. 오직 React 함수에서만 hook을 호출해야 한다.
+import styles from "./App.module.css"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    if (count === 5) {
-      console.log("This is five")
-    }
-  }, [count])
-
   return (
-    <div className="App">
-      <span>{count}</span>
-      <button onClick={() => setCount(count + 1)}>+</button>
-    </div>
+    <>
+      <div className={styles.nav}>Nav</div>
+      <div className={styles.header}>Header</div>
+      <div className={styles.listContainer}>listContainer</div>
+      <div className={styles.footer}>footer</div>
+    </>
   )
 }
 
