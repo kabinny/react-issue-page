b3d7f5947d9a64124846a8e916bf0e2c2dc3cab3
kabinny <kabinny@gmail.com>
2022-09-12 17:33:49 +0900

    filter label 만들기

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -25,14 +25,7 @@ export default function ListContainer() {
       </div>
       <OpenCloseFilters />
       <ListItemLayout className={styles.listFilter}>
-        <div className={styles.filterLists}>
-          <span>Author</span>
-          <span>Label</span>
-          <span>Projects</span>
-          <span>Milestones</span>
-          <span>Assignee</span>
-          <span>Sort</span>
-        </div>
+        <ListFilter />
       </ListItemLayout>
       <div className={styles.container}>
         <ListItem
@@ -53,6 +46,27 @@ export default function ListContainer() {
         />
       </div>
     </div>
+  )
+}
+
+function ListFilter() {
+  return (
+    <div className={styles.filterLists}>
+      <ListFilterItem>Author</ListFilterItem>
+      <ListFilterItem>Label</ListFilterItem>
+      <ListFilterItem>Projects</ListFilterItem>
+      <ListFilterItem>Milestones</ListFilterItem>
+      <ListFilterItem>Assignee</ListFilterItem>
+      <ListFilterItem>Sort</ListFilterItem>
+    </div>
+  )
+}
+
+function ListFilterItem({ onClick, children }) {
+  return (
+    <span role="button" onClick={onClick}>
+      {children} ▾
+    </span>
   )
 }
 
