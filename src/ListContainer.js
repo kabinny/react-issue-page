2a4395512ad6aa73978dabf206fd12525f95c063
kabinny <kabinny@gmail.com>
2022-09-13 21:45:17 +0900

    Pagination 컴포넌트 만들기

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -5,41 +5,56 @@ import Button from "./components/Button"
 import ListItem from "./components/ListItem"
 import ListItemLayout from "./components/ListItemLayout"
 import Modal from "./components/Modal"
+import Pagination from "./components/Pagination"
 
 export default function ListContainer() {
   const [inputValue, setInputValue] = useState("is:pr is:open")
   const [list, setList] = useState([])
+  const [page, setPage] = useState(1)
 
   return (
-    <div className={styles.listContainer}>
-      <div className={styles.topSection}>
-        <input
-          className={styles.input}
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <Button
-          style={{ fontSize: "14px", backgroundColor: "green", color: "white" }}
-        >
-          New Issue
-        </Button>
+    <>
+      <div className={styles.listContainer}>
+        <div className={styles.topSection}>
+          <input
+            className={styles.input}
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <Button
+            style={{
+              fontSize: "14px",
+              backgroundColor: "green",
+              color: "white",
+            }}
+          >
+            New Issue
+          </Button>
+        </div>
+        <OpenCloseFilters />
+        <ListItemLayout className={styles.listFilter}>
+          <ListFilter
+            onChangeFilter={(filteredData) => {
+              // 필터링된 요소에 맞게 데이터르 불러오기
+              // const data = getData('필터링된 정보')
+              // setList(data)
+            }}
+          />
+        </ListItemLayout>
+        <div className={styles.container}>
+          {list.map((listItem, index) => (
+            <ListItem key={index} badges={[{ color: "red", title: "Bug" }]} />
+          ))}
+        </div>
       </div>
-      <OpenCloseFilters />
-      <ListItemLayout className={styles.listFilter}>
-        <ListFilter
-          onChangeFilter={(filteredData) => {
-            // 필터링된 요소에 맞게 데이터르 불러오기
-            // const data = getData('필터링된 정보')
-            // setList(data)
-          }}
+      <div className={styles.paginationContainer}>
+        <Pagination
+          maxPage={10}
+          currentPage={page}
+          onClickPageButton={(number) => setPage(number)}
         />
-      </ListItemLayout>
-      <div className={styles.container}>
-        {list.map((listItem, index) => (
-          <ListItem key={index} badges={[{ color: "red", title: "Bug" }]} />
-        ))}
       </div>
-    </div>
+    </>
   )
 }
 
