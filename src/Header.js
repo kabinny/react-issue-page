29ed5a38e36db613fb66fad199f6943be7d4be49
kabinny <kabinny@gmail.com>
2022-09-14 08:25:12 +0900

    헤더에 repoNameContainer 추가

--- src/Header.js
+++ src/Header.js
@@ -7,6 +7,16 @@ export default function Header() {
   return (
     <div className={styles.header}>
       <div className={styles.buttonContainer}>
+        {/* repo name */}
+        <div className={styles.repoNameContainer}>
+          <a href="/" className={styles.userName}>
+            kabinny
+          </a>
+          <span className={styles.divider}>/</span>
+          <a href="/" className={styles.repoName}>
+            github-issue-ex
+          </a>
+        </div>
         <Button buttonStyle="default">
           Watch <div className={styles.circle}>5</div>
         </Button>
