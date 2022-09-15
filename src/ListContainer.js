453264a50bc5a10ebfe6a052dec57cc8022448c5
kabinny <kabinny@gmail.com>
2022-09-15 22:22:44 +0900

    리스트 컴포넌트와 데이터 연결

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -1,5 +1,5 @@
-import { useState } from "react"
-import cx from "clsx"
+import { useState, useEffect } from "react"
+import axios from "axios"
 import styles from "./ListContainer.module.css"
 import Button from "./components/Button"
 import ListItem from "./components/ListItem"
@@ -10,8 +10,22 @@ import OpenCloseFilters from "./components/OpenCloseFilters"
 
 export default function ListContainer() {
   const [inputValue, setInputValue] = useState("is:pr is:open")
+  const [checked, setChecked] = useState(false)
   const [list, setList] = useState([])
   const [page, setPage] = useState(1)
+
+  async function getData() {
+    const { data } = await axios.get(
+      `https://api.github.com/repos/facebook/react/issues`,
+    )
+    setList(data)
+  }
+
+  // API 로 데이터로 받아오는 작업은 useEffect 안에 넣어야 한다.
+  // 그리고 화면을 그리는 컴포넌트에 위치하면 된다.
+  useEffect(() => {
+    getData()
+  }, [])
 
   return (
     <>
@@ -35,8 +49,13 @@ export default function ListContainer() {
           />
         </ListItemLayout>
         <div className={styles.container}>
-          {list.map((listItem, index) => (
-            <ListItem key={index} badges={[{ color: "red", title: "Bug" }]} />
+          {list.map((listItem) => (
+            <ListItem
+              key={listItem.id}
+              data={listItem}
+              checked={checked}
+              onClickCheckBox={() => setChecked((checked) => !checked)}
+            />
           ))}
         </div>
       </div>
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,24 +1,35 @@
+import dayjs from "dayjs"
+import relativeTime from "dayjs/plugin/relativeTime"
 import styles from "./ListItem.module.css"
 import ListItemLayout from "./ListItemLayout"
 import Badge from "./Badge"
 
+// dayjs의 time from now를 사용하려면 RelativeTime 플러그인이 필요하다.
+dayjs.extend(relativeTime)
+
 export default function ListItem({
   checked,
-  onChangeCheckBox,
+  onClickCheckBox,
   onClickTitle,
-  badges,
+  data,
 }) {
+  const badges = data.labels
+  const state = data.state === "open" ? "opened" : "closed"
+  const date = data.state === "open" ? data.created_at : data.closed_at
+
   return (
-    <ListItemLayout>
+    <ListItemLayout checked={checked} onClick={onClickCheckBox}>
       <div>
         <div role="button" onClick={onClickTitle} className={styles.title}>
-          Issue Example
-          {badges &&
+          {data.title}
+          {badges.length > 0 &&
             badges.map((badgeProps, index) => (
               <Badge key={index} {...badgeProps} />
             ))}
         </div>
-        <div className={styles.description}># Description</div>
+        <div className={styles.description}>
+          #{data.number} {state} {dayjs(date).fromNow()} {data.user.login}
+        </div>
       </div>
     </ListItemLayout>
   )
