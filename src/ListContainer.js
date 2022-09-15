b1210676a9e855191f474cbd65bf19ce1e1b89f4
kabinny <kabinny@gmail.com>
2022-09-15 22:52:50 +0900

    페이지네이션 기능

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -8,15 +8,18 @@ import Pagination from "./components/Pagination"
 import ListFilter from "./components/ListFilter"
 import OpenCloseFilters from "./components/OpenCloseFilters"
 
+const GITHUB_API = "https://api.github.com"
+
 export default function ListContainer() {
   const [inputValue, setInputValue] = useState("is:pr is:open")
   const [checked, setChecked] = useState(false)
   const [list, setList] = useState([])
   const [page, setPage] = useState(1)
 
-  async function getData() {
+  async function getData(pageParam) {
     const { data } = await axios.get(
-      `https://api.github.com/repos/facebook/react/issues`,
+      `${GITHUB_API}/repos/facebook/react/issues`,
+      { params: { page: pageParam } },
     )
     setList(data)
   }
@@ -24,8 +27,8 @@ export default function ListContainer() {
   // API 로 데이터로 받아오는 작업은 useEffect 안에 넣어야 한다.
   // 그리고 화면을 그리는 컴포넌트에 위치하면 된다.
   useEffect(() => {
-    getData()
-  }, [])
+    getData(page)
+  }, [page])
 
   return (
     <>
